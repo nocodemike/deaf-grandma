3f4777f2de9d279e6dee33f25af56731850aa5cf
Mike Cardona <mikecardona2015@gmail.com>
2017-12-15 05:26:29 -0800

    grandma

--- deaf.rb
+++ deaf.rb
@@ -4,10 +4,9 @@ while true
 	puts 'Grandma: How are you sonny?'
 	response = gets.chomp
 
-	if response == response.downcase
+	if response != 'BYE'.upcase
 		puts 'Grandma: Huh?! Speak up, Sonny!'
 	else
-		puts 'Grandma: NO, NOT SINCE 1938!'
 		break
 	end
 end
